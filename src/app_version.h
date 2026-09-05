837c482fa758e66662c2655a9338195fcba40892
Swifttt21 <swifttt2145@gmail.com>
2026-09-05 17:53:31 +0100

    Update version resources for 3.4.1

--- src/app_version.h
+++ src/app_version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace OmniGhost {
-inline constexpr char Version[] = "3.4.1";
+inline constexpr char Version[] = "3.4.2";
 inline constexpr char BuildChannel[] = "Stable";
 inline constexpr wchar_t MainExecutable[] = L"OmniGhost.exe";
 }
