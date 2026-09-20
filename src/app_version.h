569d5631f2f275e17b6091eb787000c1db25b0ed
OmniGhost Build <build@omnighost.local>
2026-09-20 20:49:41 +0100

    chore: prepare version 1.2.3

--- src/app_version.h
+++ src/app_version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace OmniGhost {
-inline constexpr char Version[] = "1.2.2";
+inline constexpr char Version[] = "1.2.3";
 inline constexpr char BuildChannel[] = "Stable";
 inline constexpr wchar_t MainExecutable[] = L"OmniGhost.exe";
 }
